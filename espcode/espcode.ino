19b5d2c41288133af24200e0856185c37b080b8f
Ariful Islam <158015439+ariful-islam01@users.noreply.github.com>
2025-09-27 02:41:53 +0600

    Enable Bluetooth communication in espcode.ino

--- espcode/espcode.ino
+++ espcode/espcode.ino
@@ -1,30 +1,3 @@
-/*#include "BluetoothSerial.h"
-
-BluetoothSerial SerialBT;
-
-void setup() {
-  Serial.begin(115200);    // Serial to Arduino Nano
-  SerialBT.begin("GARBOT"); // Bluetooth name
-  Serial.println("ESP32 Ready for Bluetooth Commands");
-}
-
-void loop() {
-  // 1 Receive from Bluetooth → Send to Nano
-  if (SerialBT.available()) {
-    String cmd = SerialBT.readStringUntil('\n');
-    cmd.trim();
-    Serial.println("From App: " + cmd); // Debug
-    Serial.println(cmd); // Send to Nano
-  }
-
-  // 2 Receive from Nano → Send to Bluetooth
-  if (Serial.available()) {
-    String data = Serial.readStringUntil('\n');
-    data.trim();
-    Serial.println("From Nano: " + data); // Debug
-    SerialBT.println(data); // Send to App
-  }
-}*/
 #include "BluetoothSerial.h"
 
 BluetoothSerial SerialBT;
